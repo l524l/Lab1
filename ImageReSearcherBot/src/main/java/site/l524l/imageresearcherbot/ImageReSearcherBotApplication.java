b78da16523ba6c1a68ef4eab11121eb74c4867d0
l524l <perschin998@gmail.com>
2022-10-31 18:34:10 +0300

    Init

---
+++ ImageReSearcherBot/src/main/java/site/l524l/imageresearcherbot/ImageReSearcherBotApplication.java
@@ -0,0 +1,13 @@
+package site.l524l.imageresearcherbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImageReSearcherBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImageReSearcherBotApplication.class, args);
+	}
+
+}
